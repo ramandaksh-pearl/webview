cdadc3ddd97de9487ea51bff5929170611a1a28b
Raman Daksh <ramandaksh@pearlorganisation.com>
2024-08-10 14:48:56 +0530

    10-8-2024-3

--- lib/webview.dart
+++ lib/webview.dart
@@ -38,7 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
     subscription.cancel();
     super.dispose();
   }
-
+///code push
   getConnectivity() {
     subscription = Connectivity().onConnectivityChanged.listen(
           (ConnectivityResult result) async {
